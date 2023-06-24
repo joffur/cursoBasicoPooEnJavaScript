a5a76b2d1833581b35c15c3ee9aada5559f235b1
Jose Fernando Fuentes Roa <ing.josefuentes@hotmail.com>
2023-06-24 16:50:37 -0500

    Curso finalizado

---
+++ module.mjs
@@ -0,0 +1,15 @@
+import { PlatziClass } from "./main.mjs";
+
+const clase67 = new PlatziClass({
+  name: "JavaScript: orientado a objetos, basado en prototipos",
+  videoID: "5656+5asdasd3er",
+});
+
+clase67.reproducir();
+clase67.pausar();
+
+clase67.reproducir();
+clase67.pausar();
+
+clase67.reproducir();
+clase67.pausar();
